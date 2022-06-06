26e8f51e07ebc2af3e20b5c0c9a6ba484acfe1f9
Alasdair McLeay <penx@users.noreply.github.com>
2022-06-06 18:04:26 +0100

    Apply suggestions from code review
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>

--- packages/remix-gcloud-functions-gen2/jest.config.js
+++ packages/remix-gcloud-functions-gen2/jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   ...require("../../jest/jest.config.shared"),
-  displayName: "gcloud-functions-gen2",
+  displayName: "google-cloud-functions",
 };
